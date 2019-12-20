147186ead6c58e155f9eb6fbdf79fb7f0b23c240
guoxiaozhong <326167575@qq.com>
2019-12-20 16:16:42 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,23 +1,31 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import axios from 'axios'
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import store from './store'
+import router from './router'
+
+import '@/icons' // icon
+import '@/permission' // permission control
+
 import VueAMap from 'vue-amap'
 
 Vue.config.productionTip = false
 
-Vue.use(ElementUI)
-
-Vue.prototype.$axios = axios
+Vue.use(ElementUI, { locale })
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
